baa7090f1179dea5bc0fc68ec271ec2c0f7b5070
Alam <alamansari226570@gmail.com>
2024-03-19 15:37:22 +0530

    done with userSchema methods

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -2,60 +2,89 @@ import mongoose, { Schema } from "mongoose";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcryptjs";
 
-const userSchema = new Schema({
+const userSchema = new Schema(
+  {
     username: {
-        type: String,
-        required: true,
-        unique: true,
-        lowercase: true,
-        trim: true,
-        // to enable optimized search use index: true
-        index: true
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      // to enable optimized search use index: true
+      index: true,
     },
     email: {
-        type: String,
-        required: true,
-        unique: true,
-        lowercase: true,
-        trim: true,
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
     },
     fullName: {
-        type: String,
-        required: true,
-        trim: true,
-        // to enable optimized search use index: true
-        index: true
+      type: String,
+      required: true,
+      trim: true,
+      // to enable optimized search use index: true
+      index: true,
     },
     avatar: {
-        type: String, //cloudinary url
-        required: true,
+      type: String, //cloudinary url
+      required: true,
     },
-    coverImage:{
-        type: String //cloudinary url
+    coverImage: {
+      type: String, //cloudinary url
     },
     watchHistory: {
-        type: Schema.Types.ObjectId,
-        ref: "Video"
+      type: Schema.Types.ObjectId,
+      ref: "Video",
     },
     password: {
-        type: String,
-        required: [true,"Password is required"],
+      type: String,
+      required: [true, "Password is required"],
     },
     refreshToken: {
-        type: String
-    }
-
-},{timestamps:true})
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
 
 // we are not using arrow function here becuase it does not have "this" or it has no context
-userSchema.pre("save",async function(next){
-    if(!this.isModified("password")) return next()
-    this.password = bcrypt.hash(this.password,10)
-    next()
-})
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = bcrypt.hash(this.password, 10);
+  next();
+});
 
-userSchema.methods.isPasswordCorrect = async function(password){
-   return await bcrypt.compare(password,this.password)
-}
+//userSchema.methods: This refers to adding custom instance methods to a Mongoose schema. It allows you to define functions that are available on instances of the model defined by this schema.
 
-export const User = mongoose.model("User",userSchema);
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      username: this.username,
+      email: this.email,
+      fullName: this.fullName,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRE,
+    }
+  );
+};
+
+export const User = mongoose.model("User", userSchema);
