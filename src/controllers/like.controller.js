767a24d20dc0d2e50b5e3a1f090d49512fd0e951
Alam <ansarialam845922@gmail.com>
2024-03-25 20:54:27 +0530

    added like controller

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -1,33 +1,163 @@
-import mongoose, {isValidObjectId} from "mongoose"
-import {Like} from "../models/like.model.js"
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
+import mongoose, { isValidObjectId } from "mongoose";
+import { Like } from "../models/like.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 
 const toggleVideoLike = asyncHandler(async (req, res) => {
-    const {videoId} = req.params
+    const { videoId } = req.params;
     //TODO: toggle like on video
-})
+    const userId = req.user._id;
+    if (!videoId) {
+        throw new ApiError(400, "videoId required");
+    }
+    try {
+        const likeCriteria = {
+            likedBy: userId,
+            video: videoId,
+        };
+        const alreadyLiked = await Like.findOne(likeCriteria);
+        if (!alreadyLiked) {
+            const newLike = await Like.create(likeCriteria);
+            if (!newLike) {
+                throw new ApiError(400, "unable to like video");
+            }
+            return res.status(201).json(new ApiResponse(201, newLike, "Liked"));
+        }
+        const deleteLike = await Like.deleteOne(likeCriteria);
+        if (!deleteLike) {
+            throw new ApiError(400, "unable to unlike video");
+        }
+        return res
+            .status(200)
+            .json(
+                new ApiResponse(200, deleteLike, "Video like has been deleted")
+            );
+    } catch (error) {
+        throw new ApiError(500, error?.message || "Internal Server Error");
+    }
+});
 
 const toggleCommentLike = asyncHandler(async (req, res) => {
-    const {commentId} = req.params
+    const { commentId } = req.params;
     //TODO: toggle like on comment
-
-})
+    const userId = req.user._id;
+    if (!commentId) {
+        throw new ApiError(400, "commentId required");
+    }
+    try {
+        const likeCriteria = {
+            likedBy: userId,
+            comment: commentId,
+        };
+        const alreadyLiked = await Like.findOne(likeCriteria);
+        if (!alreadyLiked) {
+            const newLike = await Like.create(likeCriteria);
+            if (!newLike) {
+                throw new ApiError(400, "unable to like comment");
+            }
+            return res.status(201).json(new ApiResponse(201, newLike, "Liked"));
+        }
+        const deleteLike = await Like.deleteOne(likeCriteria);
+        if (!deleteLike) {
+            throw new ApiError(400, "unable to unlike comment");
+        }
+        return res
+            .status(200)
+            .json(
+                new ApiResponse(
+                    200,
+                    deleteLike,
+                    "comment like has been deleted"
+                )
+            );
+    } catch (error) {
+        throw new ApiError(500, error?.message || "Internal Server Error");
+    }
+});
 
 const toggleTweetLike = asyncHandler(async (req, res) => {
-    const {tweetId} = req.params
+    const { tweetId } = req.params;
     //TODO: toggle like on tweet
-}
-)
+    const userId = req.user._id;
+    if (!tweetId) {
+        throw new ApiError(400, "tweetId required");
+    }
+    try {
+        const likeCriteria = {
+            likedBy: userId,
+            tweet: tweetId,
+        };
+        const alreadyLiked = await Like.findOne(likeCriteria);
+        if (!alreadyLiked) {
+            const newLike = await Like.create(likeCriteria);
+            if (!newLike) {
+                throw new ApiError(400, "unable to like tweet");
+            }
+            return res.status(201).json(new ApiResponse(201, newLike, "Liked"));
+        }
+        const deleteLike = await Like.deleteOne(likeCriteria);
+        if (!deleteLike) {
+            throw new ApiError(400, "unable to unlike tweet");
+        }
+        return res
+            .status(200)
+            .json(
+                new ApiResponse(200, deleteLike, "tweet like has been deleted")
+            );
+    } catch (error) {
+        throw new ApiError(500, error?.message || "Internal Server Error");
+    }
+});
 
 const getLikedVideos = asyncHandler(async (req, res) => {
     //TODO: get all liked videos
-})
+    const userId = req.user._id;
+    if (!userId) {
+        throw new ApiError(400, "userId is requreied");
+    }
+    try {
+        const likedVideo = await Like.aggregate([
+            {
+                $match: {
+                    likedBy: userId,
+                },
+            },
+            {
+                $lookup: {
+                    from: "videos",
+                    localField: "video",
+                    foreignField: "_id",
+                    as: "likedVideo",
+                    pipeline: [
+                        {
+                            $project: {
+                                videoFile: 1,
+                                thumbnail: 1,
+                                title: 1,
+                                description: 1,
+                                duration: 1,
+                                views: 1,
+                                isPublished: 1,
+                                _id: 0,
+                            },
+                        },
+                    ],
+                },
+            },
+            {
+                $project: {
+                    likedVideo: 1,
+                },
+            },
+        ]);
+        console.log({ likedVideo });
+        return res
+            .status(200)
+            .json(new ApiResponse(200, likedVideo, "all liked videos"));
+    } catch (error) {
+        throw new ApiError(500, error?.message || "Internal Server Error");
+    }
+});
 
-export {
-    toggleCommentLike,
-    toggleTweetLike,
-    toggleVideoLike,
-    getLikedVideos
-}
+export { toggleCommentLike, toggleTweetLike, toggleVideoLike, getLikedVideos };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,17 +1,17 @@
 import { Router } from "express";
 import {
-  loginUser,
-  logoutUser,
-  refreshAccessToken,
-  registerUser,
-  changeCurrentPassword,
-  getCurrentUser,
-  updateAccountDetails,
-  updateUserAvatar,
-  updateUserCoverImage,
-  getUserChannelProfile,
-  getWatchHistory,
-  getAllUsers,
+    loginUser,
+    logoutUser,
+    refreshAccessToken,
+    registerUser,
+    changeCurrentPassword,
+    getCurrentUser,
+    updateAccountDetails,
+    updateUserAvatar,
+    updateUserCoverImage,
+    getUserChannelProfile,
+    getWatchHistory,
+    getAllUsers,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -19,17 +19,17 @@ const router = Router();
 
 router.route("/all").get(getAllUsers);
 router.route("/register").post(
-  upload.fields([
-    {
-      name: "avatar",
-      maxCount: 1,
-    },
-    {
-      name: "coverImage",
-      maxCount: 1,
-    },
-  ]),
-  registerUser
+    upload.fields([
+        {
+            name: "avatar",
+            maxCount: 1,
+        },
+        {
+            name: "coverImage",
+            maxCount: 1,
+        },
+    ]),
+    registerUser
 );
 router.route("/login").post(loginUser);
 
@@ -41,11 +41,11 @@ router.route("/change-password").post(verifyJWT, changeCurrentPassword);
 router.route("/current-user").get(verifyJWT, getCurrentUser);
 router.route("/update-account").patch(verifyJWT, updateAccountDetails);
 router
-  .route("/avatar")
-  .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+    .route("/avatar")
+    .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
 router
-  .route("/cover-image")
-  .patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
+    .route("/cover-image")
+    .patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
 router.route("/channel/:username").get(verifyJWT, getUserChannelProfile);
 router.route("/history").get(verifyJWT, getWatchHistory);
 
